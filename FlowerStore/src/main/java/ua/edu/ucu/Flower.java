b0d5f0aaca554a6f8e123c896da2a2d6957e5755
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-10-07 16:36:35 +0300

    first commit

---
+++ FlowerStore/src/main/java/ua/edu/ucu/Flower.java
@@ -0,0 +1,28 @@
+package ua.edu.ucu;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flower {
+    private FlowerColor color;
+    private FlowerType flowerType;
+    private double price;
+    private double sepalLengh;
+
+    public Flower(Flower flower){
+        this.color = flower.color;
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLengh = flower.sepalLengh;
+    }
+
+    public String getColor(){
+        return color.getCode();
+    }
+
+}
---
+++ Lab5/src/main/java/ua/edu/ucu/FlowerType.java
@@ -0,0 +1,2 @@
+package ua.edu.ucu;public enum FlowerType {
+}
